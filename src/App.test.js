558a38af34c0531e03cf5b8a1b39006de53199eb
Tom O'Grady <togrady01@gmail.com>
2021-09-19 11:47:06 -0400

    Update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Note Taker', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Note Taker/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
